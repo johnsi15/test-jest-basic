98c980a21da2bfefe7bdc3b79d262733a7dcf814
John Serrano <andrey_201015@hotmail.com>
2023-08-23 15:47:35 -0500

    Add Configurando supertest en el API

---
+++ api/e2e/hello.e2e.js
@@ -0,0 +1,25 @@
+const request = require('supertest');
+const createApp = require('../src/app');
+
+describe('Test for hello endpoint', () => {
+  let app = null;
+  let server = null;
+  beforeAll(() => {
+    app = createApp();
+    server = app.listen(3001);
+  });
+
+  afterAll(async () => {
+    await server.close();
+  });
+
+  describe('test for [GET]/', () => {
+    test('should return "Hello World!', async () => {
+      const response = await request(app).get('/');
+      // console.log('response', response);
+      expect(response.statusCode).toBe(200);
+      expect(response.type).toEqual('text/html');
+      expect(response.text).toEqual('Hello World!');
+    });
+  });
+});
